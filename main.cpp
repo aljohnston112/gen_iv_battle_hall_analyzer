f014cd06b0a600201260869fa628efb76e1df3da
main <aljohnston112@gmail.com>
2025-06-23 20:45:49 -0500

    Started adding moves.

--- main.cpp
+++ main.cpp
@@ -2,11 +2,10 @@
 #include <iostream>
 #include <ranges>
 
+#include "src/moves.h"
 #include "src/battle_hall_data_source.h"
 #include "src/serebii_pokemon_data_source.h"
 
 int main() {
-    print_battle_hall_pokemon();
-    print_serebii_pokemon();
     return 0;
 }
